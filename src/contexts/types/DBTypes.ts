280bf0017081915da2a8b33a5752bcadc95808c0
Jakobi Haskell <jakobi_haskell@brown.edu>
2022-04-09 16:33:53 -0400

    adds university prop

--- src/contexts/types/DBTypes.ts
+++ src/contexts/types/DBTypes.ts
@@ -3,12 +3,13 @@ export interface Person {
     lastName: string | null;
     email: string | null;
     id: string | null; 
-    
+    university: string | null;
   }
   
 export const nullPerson: Person = {
   firstName: null,
   lastName: null,
   email: null,
-  id: null
+  id: null,
+  university: null
 };
