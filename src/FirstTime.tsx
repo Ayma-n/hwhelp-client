499fb9411f23efde8725f0a27a1cb17de266f431
Ayma-n <aymanbt14@gmail.com>
2022-04-09 18:53:09 -0400

    first time form completed

--- src/FirstTime.tsx
+++ src/FirstTime.tsx
@@ -3,7 +3,9 @@ import { useNavigate, Navigate } from 'react-router-dom';
 
 import { useAuth } from './contexts/AuthContext'
 import { User } from 'firebase/auth';
-import { FormControlLabel, FormLabel, Radio, RadioGroup } from '@mui/material';
+import { FormControlLabel, FormLabel, MenuItem, Radio, RadioGroup, Select, TextField, Autocomplete, createFilterOptions, Checkbox, FormGroup } from '@mui/material';
+
+import usInstitutions from "./local-data/us_institutions_list.json"
 
 export default function FirstTime() {
 
@@ -12,17 +14,35 @@ export default function FirstTime() {
 
   const [valueA, setValueA] = useState("Student")
 
-  useEffect(() => {
+  const filterOptions = createFilterOptions({
+    matchFrom: 'any',
+    limit: 500,
+  })
 
-  }, [])
+  //useEffect(generateUniversityOptions, [])
 
-  const generateUniversityOptions = () => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 
-  }
+    console.log("THERE WAS A CHNAGE AHAHHAHAHAHA")
+    console.log(e.target.value);
 
-  const handleChange = (e : React.ChangeEvent<HTMLInputElement>) => {
-
-    if (e.target.value == "Student") {
+    const studentOptions = document.getElementById("student-options");
+    const tutorOptions = document.getElementById("tutor-options")
+    
+    if (e.target.value == "student") {
+      if (studentOptions && tutorOptions) {
+        studentOptions.classList.add("block")
+        studentOptions.classList.remove("hidden")
+        tutorOptions.classList.remove("block")
+        tutorOptions.classList.add("hidden")
+      }
+    } else if (e.target.value == "tutor") {
+      if (studentOptions && tutorOptions) {
+        tutorOptions.classList.add("block")
+        tutorOptions.classList.remove("hidden")
+        studentOptions.classList.remove("block")
+        studentOptions.classList.add("hidden")
+      }
     }
 
     setValueA((e.target as HTMLInputElement).value)
@@ -34,13 +54,35 @@ export default function FirstTime() {
       <div className="text-4xl">Welcome, {userInfo?.currentUser.displayName}!</div>
       <FormLabel>Are you using HW-Help as a:</FormLabel>
       <RadioGroup
-      defaultValue={"student"}
-      onChange={handleChange}
+        defaultValue={"student"}
+        onChange={handleChange}
       >
-        <FormControlLabel value="student" control={<Radio/>} label="Student"/>
-        <FormControlLabel value="tutor" control={<Radio/>} label="Tutor"/>
+        <FormControlLabel value="student" control={<Radio />} label="Student" />
+        <FormControlLabel value="tutor" control={<Radio />} label="Tutor" />
       </RadioGroup>
-      <FormLabel>Which university do you currently attend?</FormLabel>
+
+      <div id="student-options">
+        <FormLabel>Which university do you currently attend?</FormLabel>
+        <Autocomplete
+          disablePortal
+          filterOptions={filterOptions}
+          id="uni-list"
+          options={usInstitutions.sort()}
+          renderInput={(params) => <TextField {...params} label="College or University" />}
+        />
+      </div>
+
+      <div className="hidden" id="tutor-options">
+        <FormLabel>Select your areas of expertise:</FormLabel>
+          <FormGroup>
+            <FormControlLabel control={<Checkbox />} label="Mathematics"/>
+            <FormControlLabel control={<Checkbox />} label="Computer Science"/>
+            <FormControlLabel control={<Checkbox />} label="Physics"/>
+            <FormControlLabel control={<Checkbox />} label="Chemistry"/>
+            <FormControlLabel control={<Checkbox />} label="Economics"/>
+            <FormControlLabel control={<Checkbox />} label="Sociology"/>
+          </FormGroup>
+      </div>
     </div>
   </>)
 }
