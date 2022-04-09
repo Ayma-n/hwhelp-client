dbaa2ea345e3e34da7f779cea941866888822546
Jakobi Haskell <jakobi_haskell@brown.edu>
2022-04-09 16:18:20 -0400

    adds database context, which passes createUser function to rest of components

--- src/FirebaseInit.tsx
+++ src/FirebaseInit.tsx
@@ -1,16 +1,17 @@
-import { FirebaseApp, initializeApp } from 'firebase/app'
-import { Auth, getAuth } from 'firebase/auth'
+import { FirebaseApp, initializeApp } from "firebase/app";
+import { Auth, getAuth } from "firebase/auth";
+import { Firestore, getFirestore } from "firebase/firestore";
 
-const firebaseConfig : Object = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
-}
+const firebaseConfig: Object = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
 
-const app : FirebaseApp = initializeApp(firebaseConfig)
-export const auth : Auth = getAuth(app)
-
+const app: FirebaseApp = initializeApp(firebaseConfig);
+export const auth: Auth = getAuth(app);
+export const db: Firestore = getFirestore(app);
 export default app;
---
+++ src/contexts/DatabaseContext.tsx
@@ -0,0 +1,35 @@
+import React, { useContext, useState, useEffect, FC } from "react";
+import { db } from "../FirebaseInit";
+import {
+  collection,
+  addDoc,
+} from "firebase/firestore";
+import { useAuth } from "./AuthContext";
+import {Person, nullPerson} from "./types/DBTypes";
+
+
+const DatabaseContext: React.Context<Person> = React.createContext(nullPerson);
+
+export function useDb() {
+  return useContext(DatabaseContext);
+}
+
+export const DatabaseProvider: FC = ({ children }) => {
+  const { currentUser } = useAuth();
+  const [userInfo, setUserInfo] = useState(nullPerson);
+
+  function createUser(userObject: any) {
+    const profilesRef = collection(db, "/profiles");
+    return addDoc(profilesRef, userObject);
+  }
+
+  const value: any = {
+    createUser,
+  };
+
+  return (
+    <DatabaseContext.Provider value={value}>
+      {children}
+    </DatabaseContext.Provider>
+  );
+}
