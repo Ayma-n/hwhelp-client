0e3ace686164d55022ef1bc21211ff4046b6945e
Ayma-n <aymanbt14@gmail.com>
2022-04-09 00:10:55 -0400

    Basic design for home page

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Button from '@mui/material/Button'
+import './HomeStyle.css'
 
 type HomeProps = {
     signedIn: boolean;
@@ -7,7 +8,12 @@ type HomeProps = {
 
 export default function Home(props: HomeProps) {
   return (<>
-    <div>Home</div>
-    <Button>Sign In</Button>
+  <div id="text-and-login" className="m-auto h-screen flex flex-col text-center justify-center items-center" >
+    <div className="text-5xl mb-4">Welcome to HW-Help!</div>
+    <Button style={{
+        borderRadius: 10,
+        backgroundColor: "#22c55e",
+    }}variant="contained" className="w-30 h-16">Sign In With Google</Button>
+  </div>
   </>)
 }
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ['./src/**/*.{html,js}', './src/**/*.{html,ts}', './src/**/*.{html,tsx}'],
+  //content: ['./src/**/*.tsx'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
